cea7db211f638ae76c5c92d293cc32638ece7a06
idonnontu <a125206@gmail.com>
2023-12-28 19:48:55 +0800

    add c# app

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,336 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.pictureBox7 = new System.Windows.Forms.PictureBox();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(157, 58);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "sugar : ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(157, 109);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "beat : ";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(204, 55);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 25);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(204, 106);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 25);
+            this.textBox2.TabIndex = 3;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(83, 209);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(86, 19);
+            this.radioButton1.TabIndex = 4;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "to bed 1-1";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(286, 209);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(86, 19);
+            this.radioButton2.TabIndex = 5;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "to bed 1-2";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(83, 266);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(86, 19);
+            this.radioButton3.TabIndex = 6;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "to bed 2-1";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(286, 266);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(86, 19);
+            this.radioButton4.TabIndex = 7;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "to bed 2-2";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            this.radioButton4.CheckedChanged += new System.EventHandler(this.radioButton4_CheckedChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(131, 297);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "send data";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(220, 166);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 15);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "label3";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(12, 328);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(484, 110);
+            this.textBox3.TabIndex = 10;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(260, 297);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "listen port";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(518, 109);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(711, 236);
+            this.pictureBox1.TabIndex = 12;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox2.Location = new System.Drawing.Point(587, 138);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(30, 30);
+            this.pictureBox2.TabIndex = 13;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox3.Location = new System.Drawing.Point(667, 138);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(30, 30);
+            this.pictureBox3.TabIndex = 14;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox4.Location = new System.Drawing.Point(878, 138);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(30, 30);
+            this.pictureBox4.TabIndex = 15;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox5.Location = new System.Drawing.Point(958, 138);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(30, 30);
+            this.pictureBox5.TabIndex = 16;
+            this.pictureBox5.TabStop = false;
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox6.Location = new System.Drawing.Point(747, 151);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(30, 30);
+            this.pictureBox6.TabIndex = 17;
+            this.pictureBox6.TabStop = false;
+            // 
+            // pictureBox7
+            // 
+            this.pictureBox7.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox7.Location = new System.Drawing.Point(1044, 151);
+            this.pictureBox7.Name = "pictureBox7";
+            this.pictureBox7.Size = new System.Drawing.Size(30, 30);
+            this.pictureBox7.TabIndex = 18;
+            this.pictureBox7.TabStop = false;
+            // 
+            // imageList1
+            // 
+            this.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imageList1.ImageSize = new System.Drawing.Size(16, 16);
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(80, 23);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(113, 15);
+            this.label5.TabIndex = 20;
+            this.label5.Text = "sugar < 140mmHg";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(311, 23);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(92, 15);
+            this.label6.TabIndex = 21;
+            this.label6.Text = "heart beat > 60";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1283, 450);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.pictureBox7);
+            this.Controls.Add(this.pictureBox6);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.radioButton4);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.PictureBox pictureBox7;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,327 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Http;
+using System.Data.SqlTypes;
+using System.Xml;
+using Newtonsoft.Json.Linq;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        string bed = "";
+        List<room> hospital = new List<room>();
+        class room
+        {
+            patient pt1 = new patient();
+            patient pt2 = new patient();
+            public patient getPt1()
+            {
+                return pt1;
+            }
+            public patient getPt2()
+            {
+                return pt2;
+            }
+            public bool inDanger()
+            {
+                if(pt1.inDanger() || pt2.inDanger()) { return true; }
+                return false;
+            }
+        }
+        class patient{
+            private int sugar = 110;
+            private int beat = 80;
+            public void setSugar(int x)
+            {
+                sugar = x;
+            }
+            public void setBeat(int x)
+            {
+                beat = x;
+            }
+            public int getSugar()
+            {
+                return sugar;
+            }
+            public int getBeat()
+            {
+                return beat;
+            }
+            public bool inDanger()
+            {
+                if(sugar > 140 || beat < 60) { return true; }
+                return false;
+            }
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            await post();
+        }
+        async Task post()
+        {
+            // 创建HttpClient实例
+            using (HttpClient httpClient = new HttpClient())
+            {
+                // 准备要发送的数据
+                var postData = new FormUrlEncodedContent(new[]
+                {
+                    new KeyValuePair<string, string>("sugar", textBox1.Text),
+                    new KeyValuePair<string, string>("beat", textBox2.Text)
+                });
+                try
+                {
+                    // 发送POST请求并获取响应
+                    HttpResponseMessage response = await httpClient.PostAsync("http://192.168.125.134:1880/"+bed, postData);
+
+                    // 检查响应是否成功
+                    if (response.IsSuccessStatusCode)
+                    {
+                        string responseContent = await response.Content.ReadAsStringAsync();
+                        //label3.Text = responseContent;
+                    }
+                    else
+                    {
+                        Console.WriteLine($"失敗。{response.StatusCode}");
+                        //label3.Text = response.StatusCode.ToString();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    //label3.Text = ex.Message;
+                    Console.WriteLine($"發生異常：{ex.Message}");
+                }
+            }
+
+        }
+        async Task listenToInput()
+        {
+            string url = "http://192.168.1.21:6000/";
+            using (HttpListener listener = new HttpListener())
+            {
+                listener.Prefixes.Add(url);
+                listener.Start();
+                Console.WriteLine($"Server started at {url}");
+                label3.Text = "start listening...";
+                while (true)
+                {
+                    HttpListenerContext context = await listener.GetContextAsync();
+                    HandleRequest(context);
+                }
+            }
+        }
+        void HandleRequest(HttpListenerContext context)
+        {
+            HttpListenerRequest request = context.Request;
+            System.IO.Stream body = request.InputStream;
+            System.IO.StreamReader reader = new System.IO.StreamReader(body, request.ContentEncoding);
+            string text = reader.ReadToEnd();
+            JObject jsonObj = JObject.Parse(text);
+            string sugarValue = jsonObj["sugar"].ToString();
+            string beatValue = jsonObj["beat"].ToString();
+            string bedValue = jsonObj["bed"].ToString();
+            textBox3.Text = sugarValue + "\t";
+            textBox3.Text += beatValue + "\t";
+            textBox3.Text += bedValue;
+            modifyGraph(bedValue, sugarValue, beatValue);
+            uploadThingSpeak(bedValue, int.Parse(sugarValue), int.Parse(beatValue));
+
+            string responseString = "收到!!";  // 這是一個簡單的示例回應
+
+            // 設定回應內容類型和長度
+            HttpListenerResponse response = context.Response;
+            byte[] buffer = System.Text.Encoding.UTF8.GetBytes(responseString);
+            response.ContentType = "text/plain";
+            response.ContentLength64 = buffer.Length;
+
+            // 寫入回應到 OutputStream
+            System.IO.Stream output = response.OutputStream;
+            output.Write(buffer, 0, buffer.Length);
+            output.Close();
+
+        }
+        private async void uploadThingSpeak(string bed, int sugar, int beat)
+        {
+            string ThingSpeakWriteApiKey = "VKODHS9F6S4EV3EF"; // 替換成你的 Write API Key
+            string channel1, channel2;
+            if(bed == "1-1")
+            {
+                channel1 = "&field1=";
+                channel2 = "&field2=";
+            }
+            else if(bed == "1-2")
+            {
+                channel1 = "&field3=";
+                channel2 = "&field4=";
+            }
+            else if(bed == "2-1")
+            {
+                channel1 = "&field5=";
+                channel2 = "&field6=";
+            }
+            else
+            {
+                channel1 = "&field7=";
+                channel2 = "&field8=";
+            }
+            using (HttpClient httpClient = new HttpClient())
+            {
+                string apiUrl = "https://api.thingspeak.com/update?api_key="+ThingSpeakWriteApiKey+channel1+sugar+channel2+beat;
+
+                try
+                {
+                    HttpResponseMessage response = await httpClient.GetAsync(apiUrl);
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        string responseContent = await response.Content.ReadAsStringAsync();
+                        Console.WriteLine($"Data uploaded successfully. Response: {responseContent}");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Failed to upload data. Status code: {response.StatusCode}");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Error uploading data: {ex.Message}");
+                }
+            }
+        }
+        private void modifyGraph(string bed, string sugarValue, string beatValue)
+        {
+            Bitmap temp;
+            if (int.Parse(sugarValue) <= 140 && int.Parse(beatValue) >= 60)
+            {
+               temp = new Bitmap("..\\picture\\0-0.PNG");
+            }
+            else if(int.Parse(sugarValue) > 140 && int.Parse(beatValue) >= 60)
+            {
+                temp = new Bitmap("..\\picture\\1-0.PNG");
+            }
+            else if (int.Parse(sugarValue) <= 140 && int.Parse(beatValue) < 60)
+            {
+                temp = new Bitmap("..\\picture\\0-1.PNG");
+            }
+            else
+            {
+                temp = new Bitmap("..\\picture\\1-1.PNG");
+            }
+
+            if (bed == "1-1")
+            {
+                hospital[0].getPt1().setSugar(int.Parse(sugarValue));
+                hospital[0].getPt1().setBeat(int.Parse(beatValue));
+                pictureBox2.Image = temp;
+                if (hospital[0].inDanger())
+                {
+                    pictureBox6.Image = new Bitmap("..\\picture\\roomOn.PNG");
+                }
+                else
+                {
+                    pictureBox6.Image = new Bitmap("..\\picture\\roomOff.PNG");
+                }
+            }
+            else if(bed == "1-2")
+            {
+                hospital[0].getPt2().setSugar(int.Parse(sugarValue));
+                hospital[0].getPt2().setBeat(int.Parse(beatValue));
+                pictureBox3.Image = temp;
+                if (hospital[0].inDanger())
+                {
+                    pictureBox6.Image = new Bitmap("..\\picture\\roomOn.PNG");
+                }
+                else
+                {
+                    pictureBox6.Image = new Bitmap("..\\picture\\roomOff.PNG");
+                }
+            }
+            else if(bed == "2-1")
+            {
+                hospital[1].getPt1().setSugar(int.Parse(sugarValue));
+                hospital[1].getPt1().setBeat(int.Parse(beatValue));
+                pictureBox4.Image = temp;
+                if (hospital[1].inDanger())
+                {
+                    pictureBox7.Image = new Bitmap("..\\picture\\roomOn.PNG");
+                }
+                else
+                {
+                    pictureBox7.Image = new Bitmap("..\\picture\\roomOff.PNG");
+                }
+            }
+            else
+            {
+                hospital[1].getPt2().setSugar(int.Parse(sugarValue));
+                hospital[1].getPt2().setBeat(int.Parse(beatValue));
+                pictureBox5.Image = temp;
+                if (hospital[1].inDanger())
+                {
+                    pictureBox7.Image = new Bitmap("..\\picture\\roomOn.PNG");
+                }
+                else
+                {
+                    pictureBox7.Image = new Bitmap("..\\picture\\roomOff.PNG");
+                }
+            }
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            bed = "bed1-1";
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            bed = "bed1-2";
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            bed = "bed2-1";
+        }
+
+        private void radioButton4_CheckedChanged(object sender, EventArgs e)
+        {
+            bed = "bed2-2";
+        }
+
+        private async void button2_Click(object sender, EventArgs e)
+        {
+            await listenToInput();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox3.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox4.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox5.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox6.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox7.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox2.Image = new Bitmap("..\\picture\\0-0.PNG");
+            pictureBox3.Image = new Bitmap("..\\picture\\0-0.PNG");
+            pictureBox4.Image = new Bitmap("..\\picture\\0-0.PNG");
+            pictureBox5.Image = new Bitmap("..\\picture\\0-0.PNG");
+            pictureBox6.Image = new Bitmap("..\\picture\\roomOff.PNG");
+            pictureBox7.Image = new Bitmap("..\\picture\\roomOff.PNG");
+            hospital.Add(new room());
+            hospital.Add(new room());
+        }
+
+
+    }
+}
